b02e400cb4802227194a668c306b19a9aed5de9c
Rockstar108 <54064164+Rockstar108@users.noreply.github.com>
2023-03-20 01:59:18 +0530

    applied changes to yaml deploy file

--- cfn/ecs/cf-ecs.deploy.sh
+++ cfn/ecs/cf-ecs.deploy.sh
@@ -1,11 +1,5 @@
-# aws cloudformation create-stack --stack-name cfn-stack-hello --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region 'ap-south-1'
 STACK_NAME=cfn-stack-hello
 
-# if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
-#     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region 'ap-south-1' 
-# else
-#     aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region 'ap-south-1'
-# fi
 if aws cloudformation describe-stacks --stack-name $STACK_NAME --region 'ap-south-1' &>/dev/null 
 then
     aws cloudformation update-stack --stack-name $STACK_NAME  --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region 'ap-south-1'
