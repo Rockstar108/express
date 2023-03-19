34f139a9f8fb35d1361f654d49d494ea2fe2c0ed
Rockstar108 <54064164+Rockstar108@users.noreply.github.com>
2023-03-19 16:47:57 +0530

    Update cf-ecs.deploy.sh

--- cfn/ecs/cf-ecs.deploy.sh
+++ cfn/ecs/cf-ecs.deploy.sh
@@ -1,7 +1,7 @@
 STACK_NAME=hello-cf-ecs-ecr
 
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
-    aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --region ap-south-1 --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
+    aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region ap-south-1
 else
-    aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --region ap-soputh-1 --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM 
+    aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://cf-ecs.yaml --parameters file://cf-ecs.params.json --capabilities CAPABILITY_NAMED_IAM --region ap-south-1
 fi
