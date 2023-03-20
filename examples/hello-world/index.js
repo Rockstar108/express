1d10c7b58ad7f0089e9a44acb9d0c8fbbedd29b8
Rockstar108 <54064164+Rockstar108@users.noreply.github.com>
2023-03-20 10:52:32 +0530

    made some changes in the index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Cricket is the best thing');
 });
 
 /* istanbul ignore next */
